d6c44bd5de3235997b019d12bb359240a85374cc
Adilhon06 <adubaichik2006@gmail.com>
2024-05-28 23:54:29 +0600

    Initial commit

---
+++ app/src/main/java/com/adilhon/lesson1andoroid2/activity/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.adilhon.lesson1andoroid2.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.adilhon.lesson1andoroid2.R
+import com.adilhon.lesson1andoroid2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/adilhon/lesson1andoroid2/fragments/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.adilhon.lesson1andoroid2.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.adilhon.lesson1andoroid2.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.btnGo.setOnClickListener {
+            findNavController().navigate(
+                MainFragmentDirections.actionMainFragmentToSecondFragment(
+                    "Android"
+                )
+            )
+        }
+    }
+}
